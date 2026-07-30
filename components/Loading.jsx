d53967c74094734d0d1e9277ceb8a87cb131d461
monyettbabi22-netizen <monyettbabi22@gmail.com>
2026-07-31 04:15:26 +0700

    Loading.jsx

---
+++ components/Loading.jsx
@@ -0,0 +1,17 @@
+"use client";
+
+export default function Loading(){
+
+return(
+
+<div className="flex items-center justify-center p-20">
+
+<div className="animate-spin rounded-full h-16 w-16 border-4 border-blue-500 border-t-transparent">
+
+</div>
+
+</div>
+
+);
+
+}
