9bb950fb5a4e4260a81c729a5d78c9af7443ab91
monyettbabi22-netizen <monyettbabi22@gmail.com>
2026-07-31 06:21:13 +0700

    page.js
    Replaced static content with UploadBox component.

--- app/page.js
+++ app/page.js
@@ -1,15 +1,9 @@
+import UploadBox from "../components/UploadBox";
+
 export default function Home() {
   return (
-    <main className="min-h-screen bg-zinc-950 text-white flex items-center justify-center">
-      <div className="bg-zinc-900 p-10 rounded-2xl shadow-2xl">
-        <h1 className="text-5xl font-bold text-cyan-400">
-          CLIPFORGE AI
-        </h1>
-
-        <p className="mt-4 text-zinc-400">
-          Tailwind CSS v4 berhasil berjalan 🚀
-        </p>
-      </div>
+    <main className="min-h-screen bg-black text-white flex items-center justify-center p-8">
+      <UploadBox />
     </main>
   );
 }
