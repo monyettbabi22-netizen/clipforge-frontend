61bd6c6923e8bc5bf5e7913cfe8aec6b88de3bc4
monyettbabi22-netizen <monyettbabi22@gmail.com>
2026-07-31 04:14:58 +0700

    ClipCard.jsx

---
+++ components/ClipCard.jsx
@@ -0,0 +1,45 @@
+"use client";
+
+export default function ClipCard(){
+
+return(
+
+<div className="bg-zinc-900 rounded-xl p-5">
+
+<h2 className="text-white text-lg">
+
+Clip Result
+
+</h2>
+
+<div className="mt-4 text-gray-300">
+
+Duration :
+
+25 Seconds
+
+</div>
+
+<div className="text-gray-300">
+
+Viral Score :
+
+95%
+
+</div>
+
+<button
+
+className="mt-5 bg-green-600 hover:bg-green-500 text-white px-5 py-2 rounded-lg"
+
+>
+
+Download
+
+</button>
+
+</div>
+
+);
+
+}
