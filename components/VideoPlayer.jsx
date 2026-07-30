b9563ef3bd57a850725cd4a0cc7fbea0773d0ad7
monyettbabi22-netizen <monyettbabi22@gmail.com>
2026-07-31 06:51:27 +0700

    VideoPlayer.jsx

--- components/VideoPlayer.jsx
+++ components/VideoPlayer.jsx
@@ -1,46 +1,23 @@
 "use client";
 
-import { useEffect, useState } from "react";
-
-export default function VideoPlayer({ file }) {
-  const [videoUrl, setVideoUrl] = useState("");
-
-  useEffect(() => {
-    if (!file) {
-      setVideoUrl("");
-      return;
-    }
-
-    const url = URL.createObjectURL(file);
-
-    setVideoUrl(url);
-
-    return () => {
-      URL.revokeObjectURL(url);
-    };
-  }, [file]);
-
+export default function VideoPlayer({ video }) {
   return (
-    <div className="bg-zinc-900 rounded-2xl p-6 shadow-lg">
+    <div className="bg-zinc-900 rounded-xl p-6">
 
-      <h2 className="text-xl font-bold text-white mb-5">
+      <h2 className="text-white mb-5">
         🎬 Video Preview
       </h2>
 
-      {!file ? (
-        <div className="h-72 rounded-xl bg-zinc-800 flex items-center justify-center">
-
-          <p className="text-zinc-400">
-            Belum ada video dipilih
-          </p>
-
-        </div>
-      ) : (
+      {video ? (
         <video
-          src={videoUrl}
+          src={video}
           controls
-          className="rounded-xl w-full max-h-[500px]"
+          className="rounded-xl w-full"
         />
+      ) : (
+        <div className="text-gray-400">
+          Belum ada video dipilih
+        </div>
       )}
 
     </div>
