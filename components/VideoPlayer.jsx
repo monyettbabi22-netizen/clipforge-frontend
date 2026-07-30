8a5cabc968694c9085f4f3734e5a55574fcd2706
monyettbabi22-netizen <monyettbabi22@gmail.com>
2026-07-31 04:14:23 +0700

    VideoPlayer.jsx

---
+++ components/VideoPlayer.jsx
@@ -0,0 +1,29 @@
+"use client";
+
+export default function VideoPlayer(){
+
+return(
+
+<div className="bg-zinc-900 rounded-xl p-6">
+
+<h2 className="text-white mb-5">
+
+Preview
+
+</h2>
+
+<video
+
+controls
+
+className="rounded-xl w-full"
+
+>
+
+</video>
+
+</div>
+
+);
+
+}
